ed10fa74fd6806c9452924ce45c8da73d41c28f7
Ryan Stillings <rystills@gmail.com>
2018-04-30 16:20:17 -0400

    replace mario NN main with GVG Runner

---
+++ src/marioNeuralNet/marioNeuralNetRunner.java
@@ -0,0 +1,117 @@
+package marioNeuralNet;
+
+import java.util.Random;
+
+import core.ArcadeMachine;
+import misc.runners.GameLevelPair;
+import misc.runners.RunConfig;
+
+/*
+ * Possible Games:
+ * ALIENS, BOULDERDASH, BUTTERFLIES, CHASE, FROGS, MISSILECOMMAND,
+ * PORTALS, SOKOBAN, SURVIVEZOMBIES, ZELDA
+ */
+
+public class marioNeuralNetRunner {
+
+	public static void main(String[] args) throws Exception {
+
+		RunConfig config = new RunConfig();
+		//config.addGameLevel("qlearnMaze", 0);
+		config.addGameLevel(RunConfig.GamesTraining2014.FROGS, 1);
+
+		config.setController(SmarterMLPAgent.class.getCanonicalName());
+		config.setSaveActions(true);
+		
+		// train 100 times
+		config.setRepetitions(100);
+		runGames(config);
+		// play game visually once
+		config.setRepetitions(1);
+		runGamesVisually(config);
+	}
+
+	/**
+	 * Run the configured games with the configured controller and show the game
+	 * visually.
+	 * 
+	 * @param config
+	 *            The run configuration containing the game details.
+	 */
+	public static void runGamesVisually(RunConfig config) {
+		for (GameLevelPair<String, String[]> gameLevelPair : config
+				.getGameLevels()) {
+			for (String level : gameLevelPair.level) {
+				for (int repetition = 0; repetition < config.getRepetitions(); repetition++) {
+					String actionsFile = "actions_game_" + gameLevelPair.game
+							+ "_lvl_" + level + "_r" + repetition + "_"
+							+ RunConfig.getTimestampNow() + ".txt";
+					ArcadeMachine.runOneGame(RunConfig
+							.getGamePath(gameLevelPair.game), RunConfig
+							.getGameLevelPath(gameLevelPair.game, level), true,
+							config.getController(),
+							(config.isSaveActions()) ? actionsFile : null,
+							new Random().nextInt());
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Run the configured games with the configured controller without visual
+	 * feedback.
+	 * 
+	 * @param config
+	 *            The run configuration containing the game details.
+	 */
+	public static void runGames(RunConfig config) {
+		for (GameLevelPair<String, String[]> gameLevelPair : config
+				.getGameLevels()) {
+			ArcadeMachine.runGames(RunConfig.getGamePath(gameLevelPair.game),
+					RunConfig.getGameLevelPaths(gameLevelPair.game,
+							gameLevelPair.level), config.getRepetitions(),
+					config.getController(), config.getRecordingPathsForGame(
+							gameLevelPair.game, gameLevelPair.level));
+		}
+	}
+
+	/**
+	 * Run the configured games and play them yourself
+	 * 
+	 * @param config
+	 *            The run configuration containing the game details.
+	 */
+	public static void playGamesYourself(RunConfig config) {
+		for (GameLevelPair<String, String[]> gameLevelPair : config
+				.getGameLevels()) {
+			for (String level : gameLevelPair.level) {
+				for (int repetition = 0; repetition < config.getRepetitions(); repetition++) {
+					String actionsFile = "actions_game_" + gameLevelPair.game
+							+ "_lvl_" + level + "_r" + repetition + "_"
+							+ RunConfig.getTimestampNow() + ".txt";
+					ArcadeMachine.playOneGame(RunConfig
+							.getGamePath(gameLevelPair.game), RunConfig
+							.getGameLevelPath(gameLevelPair.game, level),
+							(config.isSaveActions()) ? actionsFile : null,
+							new Random().nextInt());
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * Replay a recorded game
+	 * 
+	 * @param readActionsFile
+	 *            The file name of the recorded game.
+	 */
+	public static void replayGame(String readActionsFile) {
+		String[] split = readActionsFile.split("_");
+		ArcadeMachine.replayGame(RunConfig.getGamePath(split[2]),
+				RunConfig.getGameLevelPath(split[2], split[4]), true,
+				readActionsFile);
+	}
+
+}
